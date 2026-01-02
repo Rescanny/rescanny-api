0b08a60cb9f6eee97ec5091adb4b5e82d99ab7b6
Gömböcz Zsolt <gombocz.zsolt@fws.hu>
2026-01-02 21:50:09 +0100

    refactor: Use invokable action to create user

---
+++ app/Domains/User/Actions/CreateUserAction.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Domains\User\Actions;
+
+use App\Domains\Subscription\SubscriptionPlan;
+use App\Models\User;
+use Laravel\Cashier\Exceptions\CustomerAlreadyCreated;
+use Laravel\Cashier\Exceptions\IncompletePayment;
+
+class CreateUserAction
+{
+    /**
+     * @throws CustomerAlreadyCreated
+     * @throws IncompletePayment
+     */
+    public function handle(string $email): User
+    {
+        $user = User::query()
+            ->create([
+                'email' => $email,
+                'locale' => app()->getLocale(),
+            ])->fresh();
+
+        $user->createAsStripeCustomer();
+
+        $user->newSubscription('default', SubscriptionPlan::Free->getPriceId())->create();
+
+        return $user;
+    }
+}
--- app/Http/Actions/Auth/MagicLinkAuthenticationAction.php
+++ app/Http/Actions/Auth/MagicLinkAuthenticationAction.php
@@ -3,19 +3,28 @@
 namespace App\Http\Actions\Auth;
 
 use App\Domains\Auth\MagicLink\UseMagicLinkAuthentication;
+use App\Domains\User\Actions\CreateUserAction;
 use App\Enums\GeneralResult;
 use App\Http\Requests\MagicLinkAuthenticationRequest;
 use App\Models\User;
+use Laravel\Cashier\Exceptions\CustomerAlreadyCreated;
+use Laravel\Cashier\Exceptions\IncompletePayment;
 use Symfony\Component\HttpFoundation\Response;
 
 class MagicLinkAuthenticationAction
 {
-    public function __invoke(MagicLinkAuthenticationRequest $request): Response
-    {
+    /**
+     * @throws CustomerAlreadyCreated
+     * @throws IncompletePayment
+     */
+    public function __invoke(
+        MagicLinkAuthenticationRequest $request,
+        CreateUserAction $createUserAction,
+    ): Response {
         $user = User::query()->where('email', $request->email)->first();
 
         if (empty($user)) {
-            $user = User::query()->create(['email' => $request->email, 'locale' => app()->getLocale()])->fresh();
+            $user = $createUserAction->handle($request->email);
         }
 
         /** @var User $user */
